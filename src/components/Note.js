497f9867b38001a00fb88387c4f2e00d48b22831
Bob Main <bobmain49@gmail.com>
2019-03-03 16:46:08 -0600

    adding jsx loop functionality for compont building

--- src/components/Note.js
+++ src/components/Note.js
@@ -7,7 +7,7 @@ function Note(props) {
   return (
     <div className="note">
       <hr/>
-      <h3> New Static Child Component:</h3>
+      <h3> New Static Child Component#{props.indexNum}:</h3>
       - <span> Message from parent: <em> "{props.fromParent}" </em> </span>
     </div>
   )
