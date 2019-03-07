51227250375f4ae611bb75a06562d79475ee025b
Bob Main <bobmain49@gmail.com>
2019-03-07 12:25:46 -0600

    added subpages

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,24 @@
 import React from "react"
+import { Link, Route } from "react-router-dom"
+import About from "./About"
+import More from "./More"
+import "../../styles/navButtons.css"
 
-function Home() {
+function Home({match}) {
+  const page = match.url === "/" ? "/home" : match.url
+  const path = window.location.pathname
   return (
-    <h1> Home Page </h1>
+    <div className="navButtons">
+      <h1> Home Page </h1>
+      <button className={path === page+'/about' ? "link current" : "link"}>
+        <Link to={page+'/about'}> About </Link>
+      </button>
+      <button className={path === page+'/more' ? "link current" : "link"}>
+        <Link to={page+'/more'}> More </Link>
+      </button>
+      <Route exact path={page+'/about'} component={About} />
+      <Route exact path={page+'/more'} component={More} />
+    </div>
   )
 }
 
