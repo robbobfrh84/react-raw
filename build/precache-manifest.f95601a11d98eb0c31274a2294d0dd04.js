71f256bb5a0fb94632ccb97a6a453d729367d11e
Bob Main <bobmain49@gmail.com>
2019-03-08 15:44:26 -0600

    added production build

---
+++ build/precache-manifest.f95601a11d98eb0c31274a2294d0dd04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "907e0d65c54f9e2549f5",
+    "url": "/static/css/main.136b1c9c.chunk.css"
+  },
+  {
+    "revision": "907e0d65c54f9e2549f5",
+    "url": "/static/js/main.907e0d65.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "4f8d3a4a5232ff329027",
+    "url": "/static/js/2.4f8d3a4a.chunk.js"
+  },
+  {
+    "revision": "3c13204266635a00a7b64b6e25354d79",
+    "url": "/index.html"
+  }
+];
